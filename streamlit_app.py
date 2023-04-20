4488f63a39fcb367bb177398653d757cd33c0975
Ashok-Avenger <131355436+Ashok-Avenger@users.noreply.github.com>
2023-04-20 18:08:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,18 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # readfile from S3 bucket
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
 
 # setting index from the list file
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Allowing user to make a selection from the picklist, giving pre-defined selection
-fruits_selected = streamlit.multiselect("Pick some fruits from the picklist below:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+# fruits_selected = streamlit.multiselect("Pick some fruits from the picklist below:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+# fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+my_fruit_list = streamlit.multiselect("Pick some fruits from the picklist below:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+my_fruit_list = my_fruit_list.loc[fruits_selected]
+
 
 # Display the data in streamlist
 streamlit.dataframe(fruits_to_show)
